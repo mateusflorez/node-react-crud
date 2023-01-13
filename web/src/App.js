a30fd7d085ed0b1b16a29b73e0cc0e2a4b5ecb01
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-13 17:45:16 -0300

    index show array todos

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+
+const arrayTodos = [{ name: "Limpar a casa", status: false }, { name: "Limpar a moto", status: false }]
+
+const Todos = ({ todos }) => {
+  return (
+    <div className="todos">
+      {todos.map(todo => {
+        return (
+          <div className="todo">
+            <p>{todo.name}</p>
+            <button><AiOutlineEdit /></button>
+            <button><AiOutlineDelete /></button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="container">
-        <div className="todos">
-        </div>
+        <Todos todos={arrayTodos} />
       </header>
     </div>
   );
